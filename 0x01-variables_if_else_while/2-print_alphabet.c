d386f863d119ed7a7bd104de47a834750802c012
nada <nadacherry7@gmail.com>
2023-06-28 05:37:13 -0700

    bruh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= z; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
