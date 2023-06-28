e77883246ff71275fdfff2732e662e2d71e00267
nada <nadacherry7@gmail.com>
2023-06-28 06:00:44 -0700

    again

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to the program
+ *
+ * return: 0 on successful execution
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
