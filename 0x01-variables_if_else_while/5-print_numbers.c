7ae152a256838b0e013f87e22bcbf053201b857c
nada <nadacherry7@gmail.com>
2023-06-28 06:17:49 -0700

    gego

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 on successful execution
+ *
+ */
+
+int main(void)
+{
+	int i;
+		for (i = 0; i <= 9; i++)
+		{
+			printf('%d', i);
+		}
+		putchar('\n');
+		return (0);
+}
