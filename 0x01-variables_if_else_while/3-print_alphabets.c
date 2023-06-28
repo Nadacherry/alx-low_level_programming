f9a25dec61e2b005c7db181aec3fe824e879d352
nada <nadacherry7@gmail.com>
2023-06-28 05:46:35 -0700

    heho

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	char C
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+		putchar(C);
+	}
+
+	putchar('\n');
+	return (0);
+}
